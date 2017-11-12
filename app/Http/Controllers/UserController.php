344dc5ebfb72edc807f169712e5dd9ec673710bd
Angel Roberto Ruiz Mendoza <angel.r.ruiz.m@gmail.com>
2017-11-12 12:40:20 -0600

    Controlador de usuario terminado

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,83 +2,101 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+
+    
     /**
-     * Display a listing of the resource.
+     * Muetra el perfil del ususario con todos sus atributos guardados en la base excepto la contraseña
      *
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function show(User $user)
     {
-        //
+        if($user==null){
+            $errors = ['No se ha encontrado el ususario especificado'];
+            return redirect()->back()->withErrors($errors);
+        }
+        return wiew('users.show');
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Muestra el formulario para editar ususario
      *
+     * @param  User $user
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function edit(User $user)
     {
-        //
+        if($user==null){
+            $errors = ['No se ha encontrado el id especificado'];
+            return redirect()->back()->withErrors($errors);
+        }
+
+        return view('users.edit');
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Actualiza la base de datos con la informacion dada.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  User $user
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function update(Request $request, User $user)
     {
-        //
+        if($user==null){
+            $errors = ['No se ha encontrado el usuario especificado'];
+            return redirect()->back()->withErrors($errors);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:50',
+            'email'=> 'required|max:250|email',
+        ]);
+
+        $userEmail=User::all()->where('email', $request->email);
+
+        if($validator->fails()){
+            return redirect()->back()->withInput($request)->withErrors($validator);
+        }
+        else{
+            if($userEmail!=null){
+                $errors = ['Ese cuenta de correo ya está asociada a otor usuario por favor elija otra'];
+                return redirect()->back()->withInput($request)->withErrors($errors);
+            }
+        }
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
+
+        session()->flash('message', 'Los cambios se han guardado con exito');
+        return redirect('/');
     }
 
     /**
-     * Display the specified resource.
+     * Cambia el valor de activa del usuario seleccionado a falso, indicanto que esta cuenta ya no existe.
      *
-     * @param  int  $id
+     * @param  User $user
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function destroy(User $user){}
     {
-        //
-    }
+        if($user==null){
+            $errors = ['No se ha encontrado el usuario especificado'];
+            return redirect()->back()->withErrors($errors);
+        }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+        $user->active = false;
+        $user->save();
+        Auth::logout();
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        session()->flash('message', 'El usuario ha sido eliminado');
+        return redirect('/home');
     }
 }
