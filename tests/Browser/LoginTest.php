1ade5e8af168f60c8d9e3345946f9566617dc9bb
Carlos Amador <carlosaam96@gmail.com>
2018-05-01 21:26:09 -0500

    Configured created .env.dusk.local, Finished LoginTest

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -10,30 +10,115 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 class LoginTest extends DuskTestCase
 {
     /**
-     * Prepare for Dusk test execution.
-     *
-     * @beforeClass
-     * @return void
-     */
-    public static function prepare()
-    {
-        static::startChromeDriver();
-    }
-
-    /**
-     * A Dusk test example.
+     * The input does not contain an email.
      *
      * @return void
      */
-    public function testExample()
+    public function testNullEmail()
     {
         $user = User::first();
         
         $this->browse(function ($browser) use ($user) {
             $browser->visit('/login')
+                    ->assertTitle('Littering')
+                    ->type('email', null)
+                    ->type('password', 'secret')
+                    ->press('#login')
+                    ->assertPathIs('/login');
+        });
+    }
+
+    /**
+     * The input does not contain a password
+     *
+     * @return void
+     */
+    public function testNullPassword()
+    {
+        $user = User::first();
+        
+        $this->browse(function ($browser) use ($user) {
+            $browser->visit('/login')
+                    ->assertTitle('Littering')
+                    ->type('email', 'user@littering.com')
+                    ->type('password', null)
+                    ->press('#login')
+                    ->assertPathIs('/login');
+        });
+    }
+
+    /**
+     * The email does not contains “@domain”.
+     *
+     * @return void
+     */
+    public function testNotValidEmail()
+    {
+        $user = User::first();
+        
+        $this->browse(function ($browser) use ($user) {
+            $browser->visit('/login')
+                    ->assertTitle('Littering')
+                    ->type('email', 'user.littering.com')
+                    ->type('password', 'secret')
+                    ->press('#login')
+                    ->assertPathIs('/login');
+        });
+    }
+
+    /**
+     * The email does not belong to any user.
+     *
+     * @return void
+     */
+    public function testNotRegisteredEmail()
+    {
+        $user = User::first();
+        
+        $this->browse(function ($browser) use ($user) {
+            $browser->visit('/login')
+                    ->assertTitle('Littering')
+                    ->type('email', 'notregistered@littering.com')
+                    ->type('password', 'secret')
+                    ->press('#login')
+                    ->assertPathIs('/login');
+        });
+    }
+
+    /**
+     * The email exists but the password is incorrect.
+     *
+     * @return void
+     */
+    public function testIncorrectPasswordLogin()
+    {
+        $user = User::first();
+        
+        $this->browse(function ($browser) use ($user) {
+            $browser->visit('/login')
+                    ->assertTitle('Littering')
+                    ->type('email', $user->email)
+                    ->type('password', 'incorrect')
+                    ->press('#login')
+                    ->assertPathIs('/login');
+        });
+    }
+
+    /**
+     * Test valid login.
+     *
+     * @return void
+     */
+    public function testValidLogin()
+    {
+        $user = User::first();
+        
+        $this->browse(function ($browser) use ($user) {
+            $browser->visit('/login')
+                    ->assertTitle('Littering')
                     ->type('email', $user->email)
                     ->type('password', 'secret')
-                    ->press('Login')
+                    ->press('#login')
                     ->assertPathIs('/usuario');
         });
     }
